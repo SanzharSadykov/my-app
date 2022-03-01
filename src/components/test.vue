8a165f9b2a59da22e6dbd0c155e264bf6005d990
SanzharSadykov <sanzharsadykov0@gmail.com>
2022-03-01 15:20:03 +0600

    qwerty

---
+++ src/components/test.vue
@@ -0,0 +1,48 @@
+<template>
+    <div>
+        <div>
+            <h1 v-if="status === 1">{{ heading }}</h1>
+            <p v-else>{{ roma }}</p>
+            <button @click="toggleSwitch()">Click me!</button>
+        </div>
+        <div v-for="item in arr" :key="item.id">
+            <p>{{ item.id }}</p>
+            <h1>{{ item.nickname }}</h1>
+            <small>{{ item.name }}</small>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "test", 
+    data() {
+        return{
+            heading: "Hello, Vue!",
+            status: 0,
+            arr: [
+                {
+                    'id': 1,
+                    'name': 'Roma',
+                    'nickname': 'roma'
+                },
+                {
+                    'id': 2,
+                    'name': 'Oljas',
+                    'nickname': 'aljas69'
+                }
+            ],
+        };
+    },
+    props: [ 'roma' ],
+    methods: {
+        toggleSwitch() {
+            if (this.status === 0) {
+                this.status = 1;
+            } else {
+                this.status = 0;
+            }
+        }
+    }
+}
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="home">
+    <test v-bind:roma="transferData"/>
+  </div>
+</template>
+
+<script>
+import test from '@/components/test.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    test
+  },
+    data() {
+        return{
+            transferData: "You go to the batman"
+        }
+    }
+}
+</script>
